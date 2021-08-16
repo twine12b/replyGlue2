ea780d3747ba1c1e9d82159c9d59b4de552a8f48
twine12b <don_twin_e@hotmail.com>
2021-08-16 21:47:10 +0100

    Need to refactor makePayment method Need to address: - [todos] - edge case - validation - dob - Calendar.class - ReadMe.md - fix controller tests

--- src/main/java/com/replyglue/demo/service/PaymentService.java
+++ src/main/java/com/replyglue/demo/service/PaymentService.java
@@ -1,7 +1,9 @@
 package com.replyglue.demo.service;
 
 import com.replyglue.demo.domain.Payment;
+import com.replyglue.demo.domain.User;
 import com.replyglue.demo.repository.PaymentRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,11 +24,24 @@ public class PaymentService {
     }
 
     public boolean makePayment(Payment payment) {
-        return registrationService.creditcardIsValid(payment.getPayment_card()) &&
-                    isValidAmount(String.valueOf(payment.getAmount()))  ?
-                         checkCardExists(String.valueOf(payment.getPayment_card()))  &&
-                                 savePayment(payment).isPresent()
-                : false;
+        if(registrationService.creditcardIsValid(payment.getPayment_card()) &&
+                isValidAmount(String.valueOf(payment.getAmount())))
+        {
+            if(checkCardExists(String.valueOf(payment.getPayment_card()))){
+                if(savePayment(payment).isPresent()){
+                    savePayment(payment);
+                    return true;
+                }
+            }
+        }
+        return false;
+
+//        return registrationService.creditcardIsValid(payment.getPayment_card()) &&
+//                    isValidAmount(String.valueOf(payment.getAmount()))  ?
+//                         checkCardExists(String.valueOf(payment.getPayment_card()))  ?
+//                                 savePayment(payment).isPresent() ? true : false  // save file
+//                         : false
+//                : false;
     }
 
     public boolean isValidAmount(String amount) {
